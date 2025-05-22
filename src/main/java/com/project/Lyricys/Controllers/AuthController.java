378a2510346ebe1488e80a0828a5acd809e99c90
nojusbuc <noahmb.nmb@gmail.com>
2025-05-22 11:30:37 +0300

    validate token endpoint

--- src/main/java/com/project/Lyricys/Controllers/AuthController.java
+++ src/main/java/com/project/Lyricys/Controllers/AuthController.java
@@ -3,27 +3,21 @@ package com.project.Lyricys.Controllers;
 import com.project.Lyricys.DTOs.LoginRequestDto;
 import com.project.Lyricys.DTOs.LoginResponseDto;
 import com.project.Lyricys.DTOs.SignupDto;
-import com.project.Lyricys.Entities.User;
-import com.project.Lyricys.Repositories.UserRepository;
 import com.project.Lyricys.Services.JwtService;
 import com.project.Lyricys.Services.RegistrationService;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final RegistrationService registrationService;
+    private final JwtService jwtService;
 
-    public AuthController(RegistrationService registrationService) {
+    public AuthController(RegistrationService registrationService, JwtService jwtService) {
         this.registrationService = registrationService;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/signup")
@@ -37,5 +31,9 @@ public class AuthController {
     public ResponseEntity<LoginResponseDto> login(
             @RequestBody @Valid LoginRequestDto dto) {
         return ResponseEntity.ok(registrationService.loginUser(dto));
+    }
+    @GetMapping("/validate")
+    public ResponseEntity<Boolean> validateToken(@RequestBody @Valid String token) {
+        return ResponseEntity.ok(jwtService.validateToken(token));
     }
 }
